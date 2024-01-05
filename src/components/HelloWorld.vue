35b4622b80dcdd95016c6716ad2bdd169671e516
ClaraPons <claragapons@gmail.com>
2024-01-05 16:59:51 +0100

    feat: clean component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,4 @@
 <script setup>
-  //
   import ImgHeader from '@/components/ImgHeader.vue'
   import ToolbarRs from '@/components/ToolbarRs.vue'
   import CarrouselCard from '@/components/CarrouselCard.vue'
@@ -18,84 +17,15 @@
 </script>
 
 <template>
-  <!-- <v-container class="fill-height">
-       <v-img height="300" src="@/assets/logo.svg" /> -->
-<!-- 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="teal"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row> -->
-      <div>
-      <v-responsive class="align-center fill-height mb-5 responsive"> 
-
+  <div>
+    <v-responsive class="align-center fill-height mb-5 responsive"> 
       <v-row class="align-center my-15">
         <v-col cols="2">
           <ToolbarRs/>
         </v-col>
 
         <v-col class="header-left" cols="3" >
-          <!-- <a class="link link--kukuri"  data-letters="Hello">Hello</a> -->
-          <h2 data-text="Bonjour">Bonjour</h2>
-          
+          <h2 data-text="Bonjour">Bonjour</h2>          
           <p  class="text-h6 pt-5 text-grey-lighten-1 font-weight-light">Je m'appelle Clara PONS, je suis développeuse Web Fullstack Junior </p>
           <v-btn
             color="teal-lighten-3"
@@ -122,7 +52,6 @@
 
     <FooterCard />
   </div>
-  <!-- </v-container>  -->
 </template>
 
 <style>
@@ -146,7 +75,6 @@ h2 {
 	font-size: 4.85vw;
 	position: relative;
 	text-transform: uppercase;
-	/* -webkit-text-stroke: 0.3vw #f7f7fe; */
 }
 
 h2::before {
@@ -223,62 +151,5 @@ h2::before {
   }
 }
 
-/* 
-.link {
-	outline: none;
-	text-decoration: none;
-	position: relative;
-	font-size: 9em;
-	line-height: 1;
-	color: #E0F2F1;
-	display: inline-block;
-  padding-top: 10px;
-}
-.link--kukuri {
-	text-transform: uppercase;
-	font-weight: 900;
-	overflow: hidden;
-	line-height: 0.75;
-	color: #E0F2F1;
-
-}
-
-.link--kukuri:hover {
-	color: #E0F2F1;
-}
-
-.link--kukuri::after {
-	content: '';
-	position: absolute;
-	height: 16px;
-	width: 100%;
-	top: 50%;
-	margin-top: -8px;
-	right: 0;
-	background: #F9F9F9;
-	transform: translate3d(-100%,0,0);
-	transition: transform 0.4s;
-	transition-timing-function: cubic-bezier(0.7,0,0.3,1);
-  
-}
-
-.link--kukuri:hover::after {
-	transform: translate3d(100%,0,0);
-}
-
-.link--kukuri::before {
-	content: attr(data-letters);
-	position: absolute;
-	z-index: 2;
-	overflow: hidden;
-	color: #80CBC4;
-	white-space: nowrap;
-	width: 0%;
-	transition: width 0.4s 0.3s;
-}
-
-.link--kukuri:hover::before {
-	width: 100%;
-} */
 
 </style>
